12be732db7c18b1ffa855281ae4c6566540a2e44
Robert Gordon <Komeuppance@gmail.com>
2020-01-21 19:40:28 -0800

    Added foriegn keys and seeds

--- data/migrations/20200121184104_cars-table.js
+++ data/migrations/20200121184104_cars-table.js
@@ -1,7 +1,7 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('cars', tbl => {
-      tbl.increments();
+  return knex.schema
+  .createTable('cars', tbl => {
+      tbl.increments('id');
       tbl.string('VIN', 128).index();
       tbl.string('Make', 128);
       tbl.string('Model', 128);
@@ -9,8 +9,18 @@ exports.up = function(knex) {
       tbl.boolean('Automatic').defaultTo(true);
       tbl.boolean('CleanTitle').defaultTo(true);
   })
+  .createTable('sales', tbl =>{
+      tbl.increments('id');
+      tbl.integer('car_id').references('id').inTable('cars').notNull().onDelete('cascade');
+      tbl.integer('Price');
+      tbl.boolean('InStock').defaultTo(true);
+      tbl.boolean('PendingSale').defaultTo(false);
+  })
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('cars');
+    return knex.schema
+    .dropTableIfExists('sales')
+    .dropTableIfExists('cars');
 };
+
